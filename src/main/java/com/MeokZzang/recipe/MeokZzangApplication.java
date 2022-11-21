bc2d70755a58b62dd4233dbdf6d1aa24d00e59b4
pporong <inane09@gmail.com>
2022-11-21 22:09:35 +0900

    환경세팅

---
+++ src/main/java/com/MeokZzang/recipe/MeokZzangApplication.java
@@ -0,0 +1,13 @@
+package com.MeokZzang.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeokZzangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeokZzangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MeokZzang/recipe/MeokZzangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MeokZzang.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeokZzangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
