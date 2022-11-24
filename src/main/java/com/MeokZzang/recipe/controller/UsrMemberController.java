51334c90cc8fd61ea2dd40449e7c669f81a73f8d
pporong <inane09@gmail.com>
2022-11-24 18:22:58 +0900

    로그인/로그아웃 기능 틀 구현

--- src/main/java/com/MeokZzang/recipe/controller/UsrMemberController.java
+++ src/main/java/com/MeokZzang/recipe/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.MeokZzang.recipe.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,4 +58,60 @@ public class UsrMemberController {
 		return ResultData.newData(joinRd, "member", member);
 	}
 	
+	// login
+	@RequestMapping("usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(HttpSession httpSession, String loginId, String loginPw) {
+		boolean isLogined = false;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+
+		if (isLogined) {
+			return ResultData.from("F-5", "!! 이미 로그인 상태입니다. !!");
+		}
+
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "아이디를 입력해주세요");
+		}
+
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "비밀번호를 입력해주세요");
+		}
+
+		Member member = memberService.getMemberByLoginId(loginId);
+
+		if (member == null) {
+			return ResultData.from("F-3", "아이디를 잘못 입력했습니다");
+		}
+
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", "!! 비밀번호가 일치하지 않습니다 !!");
+		}
+
+		httpSession.setAttribute("loginedMemberId", member.getId());
+
+		return ResultData.from("S-1", Ut.f(Ut.f("!! 반갑습니다. %s님 ! :)", member.getNickname())));
+	}
+	
+	// logout
+	@RequestMapping("usr/member/doLogout")
+	@ResponseBody
+	public ResultData doLogout(HttpSession httpSession) {
+		boolean isLogined = false;
+
+		if (httpSession.getAttribute("loginedMemberId") == null) {
+			isLogined = true;
+		}
+
+		if (isLogined) {
+			return ResultData.from("F-1", "로그아웃 상태입니다");
+		}
+
+		httpSession.removeAttribute("loginedMemberId");
+
+		return ResultData.from("S-1", "로그아웃 되었습니다 ~");
+	}
+
 }
